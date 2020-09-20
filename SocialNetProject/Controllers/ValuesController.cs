729e60635b81cffa9c21750393ef7f7e82515e10
Rostyslav Skrypnyk <skrypnik-r@ukr.net>
2020-09-20 16:01:55 +0300

    add authentication

--- SocialNetProject/Controllers/ValuesController.cs
+++ SocialNetProject/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace SocialNetProject.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
